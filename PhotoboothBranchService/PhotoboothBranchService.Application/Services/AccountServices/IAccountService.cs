ff08130eaa4a848d8309e49497efa6b36164750f
HoangNH2812 <114154942+HoangNH2812@users.noreply.github.com>
2024-07-11 06:09:50 +0700

    add get account by phone number in api and service

--- PhotoboothBranchService/PhotoboothBranchService.Application/Services/AccountServices/IAccountService.cs
+++ PhotoboothBranchService/PhotoboothBranchService.Application/Services/AccountServices/IAccountService.cs
@@ -9,6 +9,7 @@ namespace PhotoboothBranchService.Application.Services.AccountServices
     public interface IAccountService : IService<AccountResponse, CreateAccountRequestModel, AccountRegisterResponse, UpdateAccountRequestModel, AccountFilter, PagingModel>
     {
         Task<AccountResponse> GetByEmail(string Email);
+        Task<AccountResponse> GetByPhoneNumber(string phoneNumber);
         Task<AccountRegisterResponse> Register(CreateAccountRequestModel request, AccountRole userRole);
         Task<LoginResponeModel> Login(LoginRequestModel request);
         Task<LoginResponeModel> RefreshToken(RefreshTokenRequestModel request);
