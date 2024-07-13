6599035fdf3a2062e832e3e957662b54d8540e2a
HoangNH2812 <114154942+HoangNH2812@users.noreply.github.com>
2024-07-13 14:29:32 +0700

    basic dashboard as FE's requests, not testing confirm. Change now booth can not update branch id

---
+++ PhotoboothBranchService/PhotoboothBranchService.Application/DTOs/Dashboard/DashboardBackgroundResponse.cs
@@ -0,0 +1,15 @@
+﻿using PhotoboothBranchService.Application.DTOs.Background;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoboothBranchService.Application.DTOs.Dashboard
+{
+    public class DashboardBackgroundResponse
+    {
+        public int Count { get; set; }
+        public BackgroundResponse Background { get; set; }
+    }
+}
---
+++ PhotoboothBranchService/PhotoboothBranchService.Application/Services/DashboardServices/IDashboardService.cs
@@ -0,0 +1,19 @@
+﻿using PhotoboothBranchService.Application.DTOs.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoboothBranchService.Application.Services.DashboardServices
+{
+    public interface IDashboardService
+    {
+        Task<BasicBranchDashboardResponse> BasicBranchDashboard(Guid branchID);
+        Task<BasicDashboardResponse> BasicDashboard();
+        Task<List<DashboardServiceResponse>> DashboradService(Guid? branchID, DateOnly? startDate, DateOnly? endDate);
+        Task<List<DashboardSessionPackageResponse>> DashboradSessionPackage(Guid? branchID, DateOnly? startDate, DateOnly? endDate);
+        Task<List<DashboardLayoutResponse>> DashboardLayout(Guid? branchID, DateOnly? startDate, DateOnly? endDate);
+        Task<List<DashboardBackgroundResponse>> DashboardBackground(Guid? branchID, DateOnly? startDate, DateOnly? endDate);
+    }
+}
