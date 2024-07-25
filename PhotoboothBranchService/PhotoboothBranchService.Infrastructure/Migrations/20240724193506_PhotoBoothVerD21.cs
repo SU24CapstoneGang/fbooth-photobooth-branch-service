0337d9fb6ec2d61001e2c5ebd8327a3635f91e8d
Nguyễn Duy <duynt0319@gmail.com>
2024-07-25 17:06:01 +0700

    hot fix migration

---
+++ PhotoboothBranchService/PhotoboothBranchService.Infrastructure/Migrations/20240724193506_PhotoBoothVerD21.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PhotoboothBranchService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PhotoBoothVerD21 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PaymentMethods",
+                keyColumn: "PaymentMethodID",
+                keyValue: new Guid("1b4f2a3e-7d94-4119-8b6d-5c15b02848f6"));
+
+            migrationBuilder.DeleteData(
+                table: "PaymentMethods",
+                keyColumn: "PaymentMethodID",
+                keyValue: new Guid("f3b6e6b2-f90e-4f6b-8cd2-68b467afae0f"));
+
+            migrationBuilder.InsertData(
+                table: "PaymentMethods",
+                columns: new[] { "PaymentMethodID", "CreateDate", "PaymentMethodName", "Status" },
+                values: new object[,]
+                {
+                    { new Guid("1b4f2a3e-7d94-4119-8b6d-5c15b02848f6"), new DateTime(2024, 7, 24, 19, 35, 6, 522, DateTimeKind.Utc).AddTicks(2017), "VNPay", "Active" },
+                    { new Guid("f3b6e6b2-f90e-4f6b-8cd2-68b467afae0f"), new DateTime(2024, 7, 24, 19, 35, 6, 522, DateTimeKind.Utc).AddTicks(2025), "MoMo", "Active" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "PaymentMethods",
+                keyColumn: "PaymentMethodID",
+                keyValue: new Guid("1b4f2a3e-7d94-4119-8b6d-5c15b02848f6"),
+                column: "CreateDate",
+                value: new DateTime(2024, 7, 24, 18, 10, 23, 439, DateTimeKind.Utc).AddTicks(8646));
+
+            migrationBuilder.UpdateData(
+                table: "PaymentMethods",
+                keyColumn: "PaymentMethodID",
+                keyValue: new Guid("f3b6e6b2-f90e-4f6b-8cd2-68b467afae0f"),
+                column: "CreateDate",
+                value: new DateTime(2024, 7, 24, 18, 10, 23, 439, DateTimeKind.Utc).AddTicks(8654));
+        }
+    }
+}
